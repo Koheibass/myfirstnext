8e21b4c061e92a748aa448db4e3e19ebb2256556
koheis <kouheihand@yahoo.co.jp>
2023-12-26 02:26:03 +0900

    Reactで作曲家時代検索

--- pages/index.js
+++ pages/index.js
@@ -3,54 +3,88 @@ import { Inter } from 'next/font/google'
 import { HelloWorld } from '../components/HelloWorld'
 import { Link } from 'next/link'
 import React from 'react'
-
-const masterArtists = [
-  { name: 'バッハ', image: '/bach.jpg' },
-  { name: 'ベートーベン', image: '/beethoven.jpg' },
-  { name: 'モーツァルト', image: '/mozart.jpg' },
-  { name: 'ショパン', image: '/chopin.jpg' },
-  { name: 'リスト', image: '/liszt.jpg' },
-  { name: 'シューベルト', image: '/schubert.jpg' },
-]
+import { useState } from "react";
 
 export default function Home() {
-  const [artists, setArtists] = React.useState([])
-  const handleClickButton = () => {
-    alert('こんにちは！')
-    setArtists(masterArtists)
-  }
+  const masterArtist = [
+    { name: 'バッハ', era: 'Baroque' },
+    { name: 'ヴィヴァルディ', era: 'Baroque' },
+    { name: 'ヘンデル', era: 'Baroque' },
+    { name: 'ベートーベン', era: 'Classical' },
+    { name: 'モーツァルト', era: 'Classical' },
+    { name: 'ショパン', era: 'Romantic' },
+    { name: 'リスト', era: 'Romantic' },
+    { name: 'シューベルト', era: 'Classical' },
+  ];
+
+  const [showmasterArtist, setmasterArtist] = React.useState(masterArtist);
+  const [inputValue, setInputValue] = useState();
+
+  const eras = Array.from(new Set(masterArtist.map((artist) => artist.era)));
+
+  const selectera = (era) => {
+    if (era === "all") {
+      setmasterArtist(masterArtist);
+    } else {
+      const selectedmasterArtist = masterArtist.filter((artist) => artist.era === era);
+      setmasterArtist(selectedmasterArtist);
+    }
+  };
+
+  const search = (value) => {
+    if (value !== "") {
+      const serchedmasterArtist = masterArtist.filter(
+        (artist) =>
+          Object.values(artist).filter(
+            (item) =>
+              item !== undefined &&
+              item !== null &&
+              item.toUpperCase().indexOf(value.toUpperCase()) !== -1
+          ).length > 0
+      );
+      setmasterArtist(serchedmasterArtist);
+      return;
+    }
+
+    setmasterArtist(masterArtist);
+    return;
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    search(e.target.value);
+  };
 
   return (
-    <main>
-      <HelloWorld>
-        はろー！
-      </HelloWorld>
+    <div className="Home">
+      <h1>クラシック作曲家の時代検索</h1>
 
-      <HelloWorld>
-        どうもどうも！
-      </HelloWorld>
-
-      <HelloWorld >よろしくね！</HelloWorld>
-
+      { }
       <div>
-        hoge
-        <a href="/welcome">welcome?</a>
+        <h2>時代区分(era)</h2>
+        <button onClick={() => selectera("all")}>All</button>
+        {eras.map((era) => (
+          <button onClick={() => selectera(era)}>{era}</button>
+        ))}
       </div>
 
-      <button className='border rounded px-2 py-1 hover:bg-blue-200' type="button" onClick={() => handleClickButton()}>
-        こんにちは！
-      </button>
+      { }
+      <div>
+        <h4>Search</h4>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+      </div>
 
-      {
-        artists.map((artists) => {
-          return (
-            <div key={artists.name}>
-              <div>{artists.name}</div>
-            </div>
-
-          )
-        })
-      }
-    </main>
-  )
+      { }
+      {showmasterArtist.map((artist, index) => {
+        return (
+          <div key={artist.name}>
+            <p>
+              {index + 1}. {artist.name}
+            </p>
+            <p>era : {artist.era}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
